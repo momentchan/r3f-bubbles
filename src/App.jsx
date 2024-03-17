8d983186177f923f4ffcf67933c7aba51b980e6f
momentchan <mingjyunhung@gmail.com>
2024-03-17 22:19:21 +0800

    remove controls

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,9 @@ import { EffectComposer, N8AO, TiltShift2 } from "@react-three/postprocessing";
 import { useControls } from "leva";
 
 export default function App() {
-    const { blur } = useControls({
-        blur: { value: 0.1, min: 0, max: 1 }
-    })
+    // const { blur } = useControls({
+    //     blur: { value: 0.1, min: 0, max: 1 }
+    // })
 
     return (
         <Canvas shadows dpr={[1, 2]} gl={{ antialias: false }} camera={{ fov: 50, position: [0, 0, 20] }}>
@@ -18,7 +18,7 @@ export default function App() {
             <Bubbles />
             <EffectComposer disableNormalPass>
                 <N8AO aoRadius={6} intensity={2} color="red" />
-                <TiltShift2 blur={blur} />
+                <TiltShift2 blur={0.1} />
             </EffectComposer>
             <Environment preset="city" />
         </Canvas>
